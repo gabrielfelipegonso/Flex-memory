b9b61836d5f2e69ee980021c130f63618a522cda
gabrielfelipegonso <gabrielfelipegonso@gmail.com>
2024-01-31 17:46:06 -0300

    pokfposka

--- javascript/script.js
+++ javascript/script.js
@@ -5,6 +5,8 @@ start.addEventListener("click", startGame);
 const reset = document.getElementById("reset");
 reset.addEventListener("click",resetGame);
 
+var larguraDaTela = window.innerWidth;
+
 const IMG_IDS = ['icon-1', 'icon-2', 'icon-3', 'icon-4', 'icon-5', 'icon-6', 'icon-7', 'icon-8', 'icon-9', 'icon-10',  "icon-11", "icon-12", "icon-13", "icon-14", "icon-15", "icon-16", "icon-17", "icon-18", "icon-19",'icon-20', 'icon-21', 'icon-22', 'icon-23', 'icon-24'];
 
 var imagesOnGame = 12;
@@ -82,6 +84,7 @@ function teste(id) {
     }
 
 function startGame(){
+    
     opacity(0);
     
     setTimeout(function() {
@@ -108,6 +111,9 @@ function startGame(){
     reset.style.display = "inline";
     start.style.display = "none";
     emojiDoubt();
+    if (larguraDaTela<=650){
+        document.getElementsByTagName(header).style.display = "none";
+    }
     // emojiDoubt();
     }, 600);
 
@@ -132,6 +138,9 @@ function resetGame(){
     start.style.display = "inline";
     reset.style.display = "none";
     resetImages();
+    if (larguraDaTela<=650){
+        document.getElementsByTagName(header).style.display = "flex";
+    }
 
     }, 600);
 
